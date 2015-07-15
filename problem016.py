c4731f9d61f2e7021599bf7b957307df4d8a2abb
niimailtah <niimailtah@gmail.com>
2015-07-15 23:32:54 +0500

    Add some tasks for solution

---
+++ problem016.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Autor: Alexey V. Polurotov
+# e-mail: niimailtah@gmail.com
+# Common nick: Niimailtah
+# ----------------------------------------------------------------------------
+# https://projecteuler.net/problem=16
+# Power digit sum
+# Problem 16
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#
+# What is the sum of the digits of the number 21000?
