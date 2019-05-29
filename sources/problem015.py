b9d9ce01e47b209471055d686c7febec35c6a425
U-APPLIEDTECH\APolurotov <APolurotov@AT-D-AP02.appliedtech.ru>
2019-05-29 15:34:26 +0500

    added link to translated tasK

--- sources/problem015.py
+++ sources/problem015.py
@@ -19,3 +19,81 @@
 #  +--+--v  +--+--V  +--V-->  +--+--V  +--V-->  V-->-->
 #
 # How many such routes are there through a 20×20 grid?
+#
+#graph = {'0,0': ['0,1', '1,0'],
+#         '0,1': ['0,2', '1,1'],
+#         '0,2': ['1,2'],
+#         '1,0': ['1,1', '2,0'],
+#         '1,1': ['1,2', '2,1'],
+#         '1,2': ['2,2'],
+#         '2,0': ['2,1'],
+#         '2,1': ['2,2'],
+#         '2,2': []}
+
+
+def gen_diamond_graph(length):
+  result = {}
+  for i in range(length + 1):
+    for j in range(length + 1):
+      key = "{},{}".format(i, j)
+      result[key] = []
+      if j < length:
+        right = "{},{}".format(i, j + 1)
+        result[key].append(right)
+      if i < length:
+        left = "{},{}".format(i + 1, j)
+        result[key].append(left)
+  return result
+
+
+def find_path(graph, start, end, path=[]):
+  path = path + [start]
+  if start == end:
+    return path
+  if start not in graph:
+    return None
+  for node in graph[start]:
+    if node not in path:
+      newpath = find_path(graph, node, end, path)
+      if newpath:
+        return newpath
+  return None
+
+
+def find_all_paths(graph, start, end, path=[]):
+  path = path + [start]
+  if start == end:
+    return [path]
+  if start not in graph:
+    return []
+  paths = []
+  for node in graph[start]:
+    if node not in path:
+      newpaths = find_all_paths(graph, node, end, path)
+      for newpath in newpaths:
+        paths.append(newpath)
+  return paths
+
+
+def find_shortest_path(graph, start, end, path=[]):
+  path = path + [start]
+  if start == end:
+    return path
+  if start not in graph:
+    return None
+  shortest = None
+  for node in graph[start]:
+    if node not in path:
+      newpath = find_shortest_path(graph, node, end, path)
+      if newpath:
+        if not shortest or len(newpath) < len(shortest):
+          shortest = newpath
+  return shortest
+
+length = 4
+graph = gen_diamond_graph(length)
+
+for key in sorted(graph.keys()):
+  print("Graph[{}] -> {}".format(key, graph[key]))
+start, end = "{},{}".format(0, 0), "{},{}".format(length, length)
+print(len(find_all_paths(graph, start, end)))
